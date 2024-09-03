ff0c3a87004a807e2fab10512e10bf5514c33fd6
Christinne <vanessa0christinne@gmail.com>
2024-09-03 15:51:35 +0200

    refact: move classes

---
+++ TodoListAPI/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class LoginUser(BaseModel):
+    email: str
+    password: str
+
+
+class Task(BaseModel):
+    title: str
+    description: str
