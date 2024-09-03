f4e2b9b20be1f36bfa9b7645dfcdd7d824d26d69
Christinne <vanessa0christinne@gmail.com>
2024-09-03 17:43:58 +0200

    refact: add try-except-finally

--- TodoListAPI/main.py
+++ TodoListAPI/main.py
@@ -17,8 +17,9 @@ async def get_current_user(authorization: str = Header(...)):
 
     if user is None:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
-
+    
     return user
+
 
 @app.get("/message")
 async def message():
@@ -26,44 +27,65 @@ async def message():
         "Message": "Hello, World!"
     }
 
+
 @app.post("/register")
 async def register(user: User):
     cursor = db.cursor(dictionary=True)
-    cursor.execute(
-        "SELECT * FROM users WHERE email = %s", 
-        (user.email,)
-    )
-    if cursor.fetchone():
-        raise HTTPException(status_code=400, detail="Email already in use")
+    
+    try:
+        cursor.execute(
+            "SELECT * FROM users WHERE email = %s", 
+            (user.email,)
+        )
+        if cursor.fetchone():
+            raise HTTPException(status_code=400, detail="Email already in use")
+    
+        hashed = sha256_crypt.using(rounds=1000).hash(user.password)
+        token = str(uuid4())
+        
+        cursor = db.cursor()
+        cursor.execute(
+            "INSERT INTO users (name, email, password, token) VALUES (%s, %s, %s, %s)", 
+            (user.name, user.email, hashed, token)
+        )
+        db.commit()
+        
+        if cursor.rowcount == 1:
+            return {"token": token}
+        else:
+            raise HTTPException(status_code=500, detail="Error creating user")
 
-    hashed = sha256_crypt.using(rounds=1000).hash(user.password)
-    token = str(uuid4())
-    
-    cursor = db.cursor()
-    cursor.execute(
-        "INSERT INTO users (name, email, password, token) VALUES (%s, %s, %s, %s)", 
-        (user.name, user.email, hashed, token)
-    )
-    db.commit()
-    
-    if cursor.rowcount == 1:
-        return {"token": token}
-    else:
-        raise HTTPException(status_code=500, detail="Error creating user")
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        cursor.close()
+
 
 @app.post("/login")
 async def login(user: LoginUser):
     cursor = db.cursor(dictionary=True)
-    cursor.execute(
-        "SELECT * FROM users WHERE email = %s", 
-        (user.email,)
-    )
-    result = cursor.fetchone()
+    
+    try:
+        cursor.execute(
+            "SELECT * FROM users WHERE email = %s", 
+            (user.email,)
+        )
+        result = cursor.fetchone()
+    
+        if result is None or not sha256_crypt.verify(user.password, result['password']):
+            raise HTTPException(status_code=401, detail=f"Invalid email or password")
+    
+        return {"token": result['token']}
 
-    if result is None or not sha256_crypt.verify(user.password, result['password']):
-        raise HTTPException(status_code=401, detail=f"Invalid email or password")
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
 
-    return {"token": result['token']}
+    finally:
+        cursor.close()
+
 
 @app.post("/todos")
 async def create_task(task: Task, user: dict = Depends(get_current_user)):
