4d73cb6ff797884807ef06ab1dbc58d458de544b
Christinne <vanessa0christinne@gmail.com>
2025-02-14 17:36:24 +0100

    build: add Package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "RoadmapProjects",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .executable(
+            name: "MovieReservation",
+            targets: ["MovieReservation"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MovieReservation",
+            dependencies: [],
+            path: "MovieReservation/Sources",
+            exclude: [],
+            sources: ["Models", "Utils", "main.swift"]
+        )
+    ]
+)
